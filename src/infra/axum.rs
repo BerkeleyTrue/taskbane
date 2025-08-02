747ceb17025b44144b88946091a8f1d597463e8c
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-02 00:00:06 -0700

    feat: add graceful shutdown

--- src/infra/axum.rs
+++ src/infra/axum.rs
@@ -1,5 +1,10 @@
 use axum::{routing::MethodRouter, serve, Router};
+use tokio::signal;
 use tracing::info;
+
+pub fn route(path: &str, handler: MethodRouter) -> Router {
+    Router::new().route(path, handler)
+}
 
 pub async fn start_server(app: Router) {
     // Initialize tracing
@@ -8,9 +13,34 @@ pub async fn start_server(app: Router) {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     info!("Listening on port 3000");
-    serve(listener, app).await.unwrap();
+    serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
 
-pub fn route(path: &str, handler: MethodRouter) -> Router {
-    Router::new().route(path, handler)
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+    
+    info!("Initiating graceful shutdown...");
 }
