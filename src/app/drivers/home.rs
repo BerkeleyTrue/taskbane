c44d4c1242d630425eda6783628873eea846b4cb
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-01 23:53:04 -0700

    feat: add maud html macro-ing

--- src/app/drivers/home.rs
+++ src/app/drivers/home.rs
@@ -1,10 +1,22 @@
 use crate::infra::axum::route;
 use axum::routing::get;
+use maud::html;
 
 pub fn home_routes() -> axum::Router {
     route("/", get(get_home))
 }
 
-async fn get_home() -> &'static str {
-    "Welcome to the home page!"
+async fn get_home() -> maud::Markup {
+    html! {
+        (maud::DOCTYPE)
+        html {
+            head {
+                title { "Home Page" }
+            }
+            body {
+                h1 { "Welcome to the Home Page!" }
+                p { "This is a simple home page served by Axum." }
+            }
+        }
+    }
 }
