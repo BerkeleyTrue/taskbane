8348fd938a8eb47464e5610d57dcfb24d052d41d
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-06 13:30:14 -0700

    feat: share one_shot between clients

--- src/infra/hotreload.rs
+++ src/infra/hotreload.rs
@@ -15,24 +15,36 @@ use tokio_stream::{
     wrappers::{BroadcastStream, IntervalStream},
     Stream,
 };
+use tracing::info;
 
 pub fn hot_reload(on_start_rx: oneshot::Receiver<()>) -> Router {
     // convert the oneshot receiver into a broadcast channel sender
     // so that we can send a start event when the server starts
     // to all clients that are connected to the SSE endpoint
     let (tx, _) = broadcast::channel(16);
-    let tx_clone = tx.clone();
 
-    // connect the oneshot receiver to the broadcast sender
-    tokio::spawn(async move {
-        if on_start_rx.await.is_ok() {
-            let _ = tx_clone.send(());
-        }
-    });
+    // We'll share the oneshot receiver through an Arc<Mutex<Option<...>>>
+    // so it can be consumed when the first client connects
+    let on_start_rx = std::sync::Arc::new(tokio::sync::Mutex::new(Some(on_start_rx)));
 
     async fn get_hot_reload(
-        State(tx): State<broadcast::Sender<()>>,
+        State((tx, on_start_rx)): State<(
+            broadcast::Sender<()>,
+            std::sync::Arc<tokio::sync::Mutex<Option<oneshot::Receiver<()>>>>,
+        )>,
     ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+        // Start listening for the start signal when first client connects
+        let tx_clone = tx.clone();
+        let on_start_rx_clone = on_start_rx.clone();
+        tokio::spawn(async move {
+            let mut guard = on_start_rx_clone.lock().await;
+            if let Some(rx) = guard.take() {
+                if rx.await.is_ok() {
+                    info!("Server started, sending start event to clients");
+                    let _ = tx_clone.send(());
+                }
+            }
+        });
         // tx.subscribe() creates a new receiver for each client
         let start_stream = BroadcastStream::new(tx.subscribe())
             .map(|_| Ok(Event::default().event("start").data("server started")));
@@ -47,7 +59,7 @@ pub fn hot_reload(on_start_rx: oneshot::Receiver<()>) -> Router {
 
     Router::new()
         .route("/__hotreload", get(get_hot_reload))
-        // we make tx shared state
-        // so that it can be accessed by the handler
-        .with_state(tx)
+        // we make tx and on_start_rx shared state
+        // so that they can be accessed by the handler
+        .with_state((tx, on_start_rx))
 }
