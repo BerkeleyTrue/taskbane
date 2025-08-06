51a2d232a61e8ca5610ca9d5d6bed67caf051440
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-06 13:38:17 -0700

    feat: add timeout and reloading to hot reload

--- public/js/hotreload.js
+++ public/js/hotreload.js
@@ -1,20 +1,36 @@
-(function() {
-    'use strict';
+const log = (...args) => {
+    console.log('[HotReload]', ...args);
+};
 
+function initHotReload() {
+    log('Initializing hot reload...');
     const eventSource = new EventSource('/__hotreload');
 
     eventSource.addEventListener('start', function(event) {
-        console.log('Server restarted, reloading page...');
-        window.location.reload();
+        log('Server restarted, reloading page...');
+        setTimeout(() => window.location.reload(), 1000);
     });
 
     eventSource.addEventListener('heartbeat', function(event) {
-        console.log('Hot reload heartbeat');
+        log('Hot reload heartbeat');
     });
 
     eventSource.addEventListener('error', function(event) {
-        console.log('Hot reload connection error, retrying...');
+        log('Hot reload connection error, retrying...');
+        eventSource.close();
+        setTimeout(initHotReload, 1000);
     });
+  
+    eventSource.onerror = function(event) {
+        log('Hot reload error:', event);
+        eventSource.close();
+        setTimeout(initHotReload, 1000);
+    };
 
-    console.log('Hot reload script loaded');
-})();
+    window.addEventListener('beforeunload', function() {
+        eventSource.close();
+        return true;
+    });
+};
+
+setTimeout(initHotReload, 1000);
