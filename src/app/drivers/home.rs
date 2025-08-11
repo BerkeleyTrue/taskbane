eb41483cdda7c0fb8ce8e787386d935bdf622e37
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-11 14:12:55 -0700

    feat: first pass at askama templating

--- src/app/drivers/home.rs
+++ src/app/drivers/home.rs
@@ -1,24 +1,23 @@
-use crate::{app::drivers::layout::layout, infra::axum::route};
-use axum::routing::get;
-use maud::html;
+use crate::infra::axum::route;
+use askama::Template;
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+};
 
 pub fn home_routes() -> axum::Router {
     route("/", get(get_home))
 }
 
-async fn get_home() -> maud::Markup {
-    layout(html! {
-        div {
-            h1 { "Home Page!" }
-            p { "This is a simple home page served by Axum." }
-            div {
-                button
-                    class="text-white bg-rose-400 hover:bg-rose-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    onclick="initRegister()" {
-                        "Register"
-                }
-            }
-        }
-        script src="/public/js/passkey.js" {}
-    })
+#[derive(Debug, Clone, Template)]
+#[template(path = "index.html")]
+struct Home {
+    title: String,
+}
+
+async fn get_home() -> Result<impl IntoResponse, axum::Error> {
+    let templ = Home {
+        title: "Taskbane".to_string(),
+    };
+    Ok(Html(templ.render()?))
 }
