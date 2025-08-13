960ed4a579b6033c5c717d329d33c5183f64b630
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-12 22:53:21 -0700

    feat(home): use username from form

--- public/js/passkey.js
+++ public/js/passkey.js
@@ -1,11 +1,16 @@
 async function initRegister() {
+  const username = document.getElementById('username').value;
+  if (!username) {
+    alert('Please enter a username');
+    return;
+  }
   const res = await fetch('/auth/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      username: 'exampleUser',
+      username,
     }),
   });
 
