c945ab380e8dab288d5a1d0d54315f4738c8c72c
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-15 22:56:37 -0700

    feat: passkey script improvements

--- public/js/passkey.js
+++ public/js/passkey.js
@@ -1,10 +1,11 @@
-async function initRegister() {
+async function initRegister(e) {
+  e.preventDefault();
   const username = document.getElementById('username').value;
   if (!username) {
     alert('Please enter a username');
     return;
   }
-  const res = await fetch('/auth/register', {
+  const creds = await fetch('/auth/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -12,7 +13,27 @@ async function initRegister() {
     body: JSON.stringify({
       username,
     }),
-  });
+  })
+    .then((res) => res.json())
+    .then((credOptions) => credOptions.publicKey)
+    .then(x => (console.log('credOptions: ', x), x))
+    .then((publicKey) => ({
+      ...publicKey,
+      challenge: new TextEncoder().encode(publicKey.challenge).buffer,
+      user: {
+        ...publicKey.user,
+        id: new TextEncoder().encode(publicKey.user.id).buffer,
+      },
+    }))
+    .then((publicKey) => {
+      return navigator.credentials.create({
+        publicKey,
+      });
+    })
+    .catch(err => {
+      console.error('Error during registration:', err);
+      alert('Registration failed. Please try again.');
+    });
 
-  console.log("res", res);
+  console.log('creds: ', creds);
 }
