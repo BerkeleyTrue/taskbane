aed1a5ac30c6020312e343439fc9fbda72ab7e34
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-27 14:34:26 -0700

    fix(task/router): redirect tasks to task

--- src/app/drivers/task.rs
+++ src/app/drivers/task.rs
@@ -1,10 +1,18 @@
-use axum::{middleware, response::IntoResponse, routing, Router};
+use axum::{
+    middleware,
+    response::{IntoResponse, Redirect},
+    routing, Router,
+};
 
 use crate::infra::{auth::authentication_middlewared, error::AppError};
 
 pub fn task_routes() -> axum::Router {
     Router::new()
-        .route("/tasks", routing::get(get_task))
+        .route("/task", routing::get(get_task))
+        .route(
+            "/tasks",
+            routing::get(async || Redirect::permanent("/task")),
+        )
         .layer(middleware::from_fn(authentication_middlewared))
 }
 
